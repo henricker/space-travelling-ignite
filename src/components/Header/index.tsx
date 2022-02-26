fef6989092ecda56d51599fb0dcd87766af4c648
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-02-26 08:34:42 -0300

    feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import { ReactElement } from 'react';
+import styles from './header.module.scss';
+
+export default function Header(): ReactElement {
+  return (
+    <header className={styles.header}>
+      <div className={styles.contentHeader}>
+        <img src="header/logo.svg" alt="logo" />
+      </div>
+    </header>
+  );
 }
