d4b3f33e686afc9752ab666299312b4b27cf389f
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-02-26 09:00:46 -0300

    feat: add style to page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
+import { ReactElement } from 'react';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import posts from '../mocks/posts.json';
 
 interface Post {
   uid?: string;
@@ -24,9 +26,31 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): ReactElement {
+  return (
+    <div className={styles.container}>
+      <ul className={styles.posts}>
+        {posts.map(post => (
+          <li>
+            <p className={styles.title}>{post.data.title}</p>
+            <p className={styles.preview}>{post.data.subtitle}</p>
+            <div className={styles.info}>
+              <div className={styles.info_option}>
+                <img src="calendar.svg" alt="calendar" />
+                <time>{post.first_publication_date}</time>
+              </div>
+              <div className={styles.info_option}>
+                <img src="user.svg" alt="calendar" />
+                <p>{post.data.author}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <a className={styles.loadPosts}>Carregar mais posts</a>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
