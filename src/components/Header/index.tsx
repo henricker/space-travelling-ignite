07eded995415c57e70cfbd152a853940e97496ad
Henrique Vieira <henriquevieira@alu.ufc.br>
2022-02-27 12:02:05 -0300

    refactor: using tag <a/> inside Link component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,24 @@
 import { ReactElement } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from './header.module.scss';
 
 export default function Header(): ReactElement {
   return (
     <header className={styles.header}>
       <div className={styles.contentHeader}>
-        <img src="header/logo.svg" alt="logo" />
+        <Link href="/">
+          <a>
+            <Image
+              src="/header/logo.svg"
+              alt="logo"
+              width={238.62}
+              height={25.63}
+              className={styles['logo-img']}
+            />
+          </a>
+          {/* <img src="header/logo.svg" alt="logo" /> */}
+        </Link>
       </div>
     </header>
   );
